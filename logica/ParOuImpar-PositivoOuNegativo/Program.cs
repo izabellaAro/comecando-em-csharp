fdc846659d0551a80b70d100e9b295676ad26385
Izabella Aro <izabella.aro20@gmail.com>
2024-01-03 14:38:08 -0300

    Adiciona exercicio que verifica se valor par ou impar e positivo ou negativo

---
+++ logica/ParOuImpar-PositivoOuNegativo/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("*********************************************************");
+Console.WriteLine("Descubra se o valor é Par ou Impar e Positivo ou Negativo");
+Console.WriteLine("*********************************************************");
+Console.WriteLine();
+
+Console.WriteLine("Digite um valor qualquer: ");
+int valor = int.Parse(Console.ReadLine());
+
+if (valor % 2 == 0) 
+{
+    Console.WriteLine("O valor informado é par");
+} else
+{
+    Console.WriteLine("O valor informado é ímpar");
+}
+
+if (valor < 0) 
+{ 
+    Console.WriteLine("O valor informado é negativo");
+} else if (valor > 0) 
+{
+    Console.WriteLine("O valor informado é positivo"); 
+}else
+{
+    Console.WriteLine("O valor informado é neutro");
+}
